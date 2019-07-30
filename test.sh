8aa901b934fe2e68d2c9856985ca2401fe5affaa
Cédric <nope-mcnopeface@asdf.org>
2019-07-30 14:20:39 +0200

    Added test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ./secret.env
+
+sudo docker run                              \
+    --rm -it                                    \
+    --volume "$DATA_VOLUME":/rootfs:ro       \
+    --env RESTIC_PASSWORD="$RESTIC_PASSWORD" \
+    --env S3_ACCESS_KEY="$S3_ACCESS_KEY"     \
+    --env S3_ENDPOINT="$S3_ENDPOINT"         \
+    --env S3_PATH="$S3_PATH"                 \
+    --env S3_REGION="$S3_REGION"             \
+    --env S3_SECRET_KEY="$S3_SECRET_KEY"     \
+    s3backup:"$(git rev-parse --abbrev-ref HEAD)" backup "$@"
