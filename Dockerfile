b0821df3c213aad31fe6edb95c99fee31a2ed370
Cédric HT <altaris@users.noreply.github.com>
2025-02-10 14:02:58 +0900

    Restic mounts

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,16 @@
 FROM alpine:latest
 
-# Metadatas
-MAINTAINER Cédric HT
-
-# Package installation through apk
 RUN apk add                                                             \
     --no-cache                                                          \
     --repository http://dl-cdn.alpinelinux.org/alpine/edge/community    \
     --repository http://dl-cdn.alpinelinux.org/alpine/edge/main         \
     --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing      \
     --update                                                            \
-    ca-certificates restic rclone
+    ca-certificates restic rclone fuse
 
-# Setup executables
-ENV PATH /app:$PATH
+ENV PATH=/app:$PATH
 COPY backup /app/
 
-# Initial state
 WORKDIR /
 ENTRYPOINT ["backup"]
 CMD [""]
--- test.sh
+++ test.sh
@@ -3,14 +3,23 @@
 # shellcheck disable=SC1091
 . ./secret.env
 
-sudo docker run                              \
-    --rm -it                                 \
-    --volume "$DATA_VOLUME":/rootfs          \
-    --env RCLONE_MAX_TRY=100                 \
-    --env RESTIC_PASSWORD="$RESTIC_PASSWORD" \
-    --env S3_ACCESS_KEY="$S3_ACCESS_KEY"     \
-    --env S3_ENDPOINT="$S3_ENDPOINT"         \
-    --env S3_PATH="$S3_PATH"                 \
-    --env S3_REGION="$S3_REGION"             \
-    --env S3_SECRET_KEY="$S3_SECRET_KEY"     \
-    s3backup:"$(git rev-parse --abbrev-ref HEAD)" "$@"
+MOUNT_POINT=./test/mnt/s3backup-dev/restic
+mkdir -p "$MOUNT_POINT"
+
+
+sudo docker run                                 \
+    --rm -it                                    \
+    --volume "$(pwd)":/rootfs:ro                \
+    --mount type=bind,src="$MOUNT_POINT",dst=/mnt/restic,bind-propagation=shared \
+    --network s3                                \
+    --name s3backup-dev                         \
+    --env RCLONE_MAX_TRY=5                      \
+    --env RESTIC_PASSWORD="$RESTIC_PASSWORD"    \
+    --env S3_ACCESS_KEY="$S3_ACCESS_KEY"        \
+    --env S3_ENDPOINT="$S3_ENDPOINT"            \
+    --env S3_PATH="$S3_PATH"                    \
+    --env S3_REGION="$S3_REGION"                \
+    --env S3_PROVIDER="$S3_PROVIDER"            \
+    --env S3_SECRET_KEY="$S3_SECRET_KEY"        \
+    --privileged \
+    altaris/s3backup:arm64 "$@"
